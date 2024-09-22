b1bcb665ff77326dd76856a944ea3c47b35e24ec
Bonolo Lebepe <125119232+nolly-B@users.noreply.github.com>
2024-09-22 20:14:34 +0200

    update task 1 week 14

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,4 +4,13 @@ const githubService = axios.create({
   baseURL: "",
 });
 
+export const fetchUserData = async (username) => {
+  try {
+    const response = await githubService.get(`/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export default githubService;
